078ec9437cefa0f8f9841c9f85989362050ada72
jackson.magnabosco <jacksonmagnabosco@hotmail.com>
2019-07-12 10:11:53 -0300

    atualização

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screens/home_screen.dart';
-import 'package:loja_virtual/screens/login_screen.dart';
-import 'package:loja_virtual/screens/signup_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:loja_virtual/models/cart_model.dart';
 
@@ -14,22 +13,48 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScopedModel<UserModel>(
       model: UserModel(),
-      child: ScopedModelDescendant<UserModel>(
-          builder: (context, child, model){
-            return ScopedModel<CartModel>(
-              model: CartModel(model),
-              child: MaterialApp(
-                  title: "Flutter's Clothing",
-                  theme: ThemeData(
-                      primarySwatch: Colors.blue,
-                      primaryColor: Color.fromARGB(255, 4, 125, 141)
-                  ),
-                  debugShowCheckedModeBanner: false,
-                  home: HomeScreen()
-              ),
-            );
-          }
+      child: ScopedModelDescendant<UserModel>(builder: (context, child, model) {
+        return ScopedModel<CartModel>(
+          model: CartModel(model),
+          child: MaterialApp(
+              title: "Flutter's Clothing",
+              theme: ThemeData(
+                  primarySwatch: Colors.blue,
+                  primaryColor: Color.fromARGB(255, 4, 125, 141)),
+              debugShowCheckedModeBanner: false,
+              home: Splash()),
+        );
+      }),
+    );
+  }
+}
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: FlareActor(
+            "assets/foguete.flr",
+            animation: "success",
+          ),
+        ),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 5)).then((_) {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => HomeScreen()));
+    });
   }
 }
